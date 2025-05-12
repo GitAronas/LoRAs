74fb1eedc38b9f0c6a764642666554db2b7ff1a6
Amar Doshi <97429962+GitAronas@users.noreply.github.com>
2025-05-12 13:13:50 +0530

    Add files via upload

---
+++ Emily Wickersham/train.bat
@@ -0,0 +1,39 @@
+accelerate launch ^
+  --mixed_precision bf16 ^
+  --num_cpu_threads_per_process 1 ^
+  sd-scripts/flux_train_network.py ^
+  --pretrained_model_name_or_path "C:\Applications\fluxgym\models\unet\flux1-dev.sft" ^
+  --clip_l "C:\Applications\fluxgym\models\clip\clip_l.safetensors" ^
+  --t5xxl "C:\Applications\fluxgym\models\clip\t5xxl_fp16.safetensors" ^
+  --ae "C:\Applications\fluxgym\models\vae\ae.sft" ^
+  --cache_latents_to_disk ^
+  --save_model_as safetensors ^
+  --sdpa --persistent_data_loader_workers ^
+  --max_data_loader_n_workers 2 ^
+  --seed 42 ^
+  --gradient_checkpointing ^
+  --mixed_precision bf16 ^
+  --save_precision bf16 ^
+  --network_module networks.lora_flux ^
+  --network_dim 4 ^
+  --optimizer_type adafactor ^
+  --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" ^
+  --split_mode ^
+  --network_args "train_blocks=single" ^
+  --lr_scheduler constant_with_warmup ^
+  --max_grad_norm 0.0 ^
+  --learning_rate 8e-4 ^
+  --cache_text_encoder_outputs ^
+  --cache_text_encoder_outputs_to_disk ^
+  --fp8_base ^
+  --highvram ^
+  --max_train_epochs 10 ^
+  --save_every_n_epochs 4 ^
+  --dataset_config "C:\Applications\fluxgym\outputs\emily-wickersham\dataset.toml" ^
+  --output_dir "C:\Applications\fluxgym\outputs\emily-wickersham" ^
+  --output_name emily-wickersham ^
+  --timestep_sampling shift ^
+  --discrete_flow_shift 3.1582 ^
+  --model_prediction_type raw ^
+  --guidance_scale 1 ^
+  --loss_type l2 ^
